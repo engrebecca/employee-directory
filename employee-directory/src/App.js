f253bbab2ac98bd3603f8421f26a159d250510de
reng <reng412@gmail.com>
2020-11-13 16:22:49 -0800

    Add function to sort fields alphabetically

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -7,9 +7,12 @@ import Employees from "./employees.json"
 import Directory from "./components/Directory";
 import EmployeeContext from "./utils/employeeContext";
 
+const fields = [{ key: "firstname", text: "First Name" }, { key: "lastname", text: "Last Name" }, { key: "position", text: "Position" }, { key: "department", text: "Department" }, { key: "email", text: "Email" }]
+
 function App() {
     const [employees, setEmployees] = useState([]);
     const [filter, setFilter] = useState("")
+    const [sortField, setSortField] = useState("firstname");
 
     useEffect(() => {
         setEmployees(Employees)
@@ -29,10 +32,20 @@ function App() {
         <div>
             {/* <Navbar></Navbar> */}
             <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+            <select value={sortField} onChange={(e) => setSortField(e.target.value)}>
+                {fields.map(({ key, text }) => (<option key={key} value={key}>{text}</option>))}
+            </select>
             <Wrapper>
                 <Header></Header>
                 {/* Section that renders all employees from employees.json */}
-                <EmployeeContext.Provider value={employees.filter((employee) => filterRegExp.test(employee.firstname))}>
+                <EmployeeContext.Provider value={employees
+                    .filter((employee) => filterRegExp.test(employee.firstname))
+                    .sort((a, b) => {
+                        if (a[sortField] < b[sortField]) return -1
+                        if (a[sortField] > b[sortField]) return 1
+                        return 0
+                    })
+                }>
                     <Directory></Directory>
                 </EmployeeContext.Provider>
             </Wrapper>
